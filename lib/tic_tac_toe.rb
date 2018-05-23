1610e68faa56109fb06a5c8907c137495c844790
Trevor Russell <truss.42@gmail.com>
2018-05-23 06:13:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,7 +28,7 @@ class TicTacToe
   def position_taken?(index)
     @board[index] != " " && @board[index] != ""
   end
-  def valid_move?(board, index)
+  def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
   def turn(board)
