4423f9f5d1a542d6cf6260182a2a3d450aea6a9b
Trevor Russell <truss.42@gmail.com>
2018-05-23 05:23:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,5 +42,13 @@ class TicTacToe
       turn
     end
   end
-  
+  def turn_count(board)
+    turn = 0
+    board.each do |space|
+      if space == "X" || space == "O"
+        turn += 1
+      end
+    end
+    return turn
+  end
 end 
