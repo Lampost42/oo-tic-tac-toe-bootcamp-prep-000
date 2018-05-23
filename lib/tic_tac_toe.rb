021aee20698e64f423dabe428ae373df442e8aff
Trevor Russell <truss.42@gmail.com>
2018-05-23 05:22:56 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,15 +12,15 @@ class TicTacToe
   [1,4,7],
   [2,5,8]
   ]
-  def display_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     puts "-----------"
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-  def input_to_index(user_input)
-    @index = user_input.to_i - 1
+  def input_to_index
+    @index = @input.to_i - 1
   end
   def move
     @board[@index] = @current_player
@@ -31,4 +31,16 @@ class TicTacToe
   def valid_move?
     @index.between?(0,8) && !position_taken?
   end
+  def turn
+    puts "Please enter 1-9:"
+    @input = gets.strip
+    input_to_index
+    if valid_move?
+      move
+      display_board
+    else
+      turn
+    end
+  end
+  
 end 
